d74cd75355f6bcb65e0e1ef0b00a80d2712a2921
Mantas Visockis <mantas.visockis@insoft.lt>
2019-02-25 15:04:36 +0200

    Set up Maven

---
+++ main-app/src/main/java/lt/m33t/main/M33tMainApp.java
@@ -0,0 +1,12 @@
+package lt.m33t.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M33tMainApp {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(M33tMainApp.class, args);
+    }
+}
